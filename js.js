2a782ec7d776360798ede0a5aaaaa652efe81c9f
nmnn33 <tommy123tran@gmail.com>
2021-12-06 00:50:44 +0200

    432342

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 //Tänne käyttäjän tiedot
-const todoObjectList = [];
+var todoObjectList = [];
 //Luokka Todo_Class, jonka sisälle funktioita
 class Todo_Class {
     constructor(item) {
@@ -82,8 +82,59 @@ class Todo_Class {
         todoObjectList.splice(selecteddelIndex, 1);
         myTodoList.display();
     }
+    //Kun sivusto latautuu, tulostetaan lista localstoragesta.
+    displayLoad() {
+        console.log("toimii displayLoad()");
+        //Pyyhitään ul elementin sisältö, jotta rakennetaan uusi lista
+        $("#myUl").html("");
+        var aktiviteetti = JSON.parse(localStorage.getItem("data"));
+        console.log(aktiviteetti);
+        //Jokaista objektia kohti, luodaan li elementti, jossa delete roskis nappi
+        aktiviteetti.forEach((object_item) => {
+            //li elementtiin aktiviteetin nimi ja id saadaan todoObject:sta, sama delete napille
+            const liElement = $('<li>' + object_item.todoText + '</li>');
+            const delBtn = $("<i></i>");
 
+            liElement.attr('data-id', object_item.id);
+            delBtn.attr('data-id', object_item.id);
+            delBtn.addClass("fas fa-trash-alt");
+            //Lisätään li elementtiin delete nappi
+            liElement.append(delBtn);
+            //Kun painetaan roskista, poistuu roskis elementti
+            delBtn.on("click", function (event) {
+                const deleteId = $(event.target).attr("data-id");
+                myTodoList.deleteElement(deleteId);
+            })
+            //Li elemenetti merkataan tehdyksi / ei tehdyksi
+            liElement.on("click", function (event) {
+                const selectedId = $(event.target).attr("data-id");
+                myTodoList.done_undone(selectedId);
+            })
+            //Jos on tehtynä aktivitetti, merkataan se checked
+            if (object_item.isDone) {
+                liElement.addClass("checked")
+            }
+            $("#myUl").append(liElement);
+        })
+        //Lisätään localstoragessa oleva objektimme meidän todoObjectList, jotta display() toimii
+        todoObjectList = aktiviteetti.slice();
+    }
 }
+
+//Tallennetaan todoObjectList local storageen nappia painaen
+function save() {
+    console.log("tallennetaan...");
+    console.log(todoObjectList);
+    localStorage.setItem("data", JSON.stringify(todoObjectList));
+    /*Testattiin
+    var dataV = JSON.parse(localStorage.getItem("data"));
+    console.log("täälyä tutlatlta");
+    console.log(dataV);
+    */
+}
+
+
+
 //Muuttujia ja constantteja
 const lista = $("#myUl");
 const nappi = $("#btn");
@@ -93,4 +144,6 @@ myTodoList = new Todo_Class(lista);
 //Document ready, eventhandlereille
 $(document).ready(function () {
     $("#btn").click(myTodoList.add);
+    $("#tallennus").click(save);
+    myTodoList.displayLoad();
 });
